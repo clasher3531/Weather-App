16018ed4856f05eea35ab8a33da7887c23fabc83
C_L_A_S_H_E_R <nikhil.a.joshi@capgemini.com>
2021-04-03 17:03:48 +0530

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Defines path for express config
 const publicDirectoryPath = path.join(__dirname,'../public');
@@ -91,6 +92,6 @@ app.get('*',(req,res) => {
     })
 })
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('Server is running on port 3000');
 })
