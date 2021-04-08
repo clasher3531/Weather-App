6ed7722a788f645175d983167e78671779768d01
Nikhil Joshi <nikhil.a.joshi@capgemini.com>
2021-04-08 16:52:21 +0530

    Adding Auto Geolocation

--- public/js/bundled/app.js
+++ public/js/bundled/app.js
@@ -1,3 +1,4 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const cookie = require('./cookie'); //require cookie 
 
 // Selecting the elements
@@ -64,3 +65,32 @@ weatherForm.addEventListener('submit',(e) => {
     cookie.setCookie('location',location,cookieExpiry);
     getInfo(location);
 })
+},{"./cookie":2}],2:[function(require,module,exports){
+function setCookie(cname, cvalue, exmins) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exmins*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+module.exports = {
+    setCookie : setCookie,
+    getCookie : getCookie
+}
+},{}]},{},[1]);
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const geocode = (location,callback) => {
 
     const geocodeurl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(location)+'.json?access_token=pk.eyJ1Ijoiam9zaGluaWtoaWwzNTMxIiwiYSI6ImNrbWFmcWYxbjFyaHEyb3FsYXh4dHlneWIifQ.yEYRAbSvDTnVHE3_EDJmEA&limit=1'
-    request({'url' : geocodeurl,json : true},(error,{body}) => {
+    request({'url' : geocodeurl,json : true},(error,{body}={}) => {
 
         if(error){
             callback('Unable to connect the location services',undefined);
